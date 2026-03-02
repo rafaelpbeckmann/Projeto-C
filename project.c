30a657788bb9ddf8c8abcba5f47be2fee015ab53
rafaelpbeckmann <rafaelpbeckmann@gmail.com>
2026-03-02 21:44:50 +0000

    jogo funcional (finalmente)

--- project.c
+++ project.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main() {
     int numeroSecreto;
     int palpite;
     int tentativas = 0;
+
     srand(time(NULL));
     numeroSecreto = rand() % 100 + 1;
+
     do {
-    printf ("tente acertar o numero de 1 a 100!: ");
-    scanf ("%d", &palpite);
+        printf("tente acertar o numero de 1 a 100!: ");
+        scanf("%d", &palpite);
         tentativas++;
-    if (palpite == numeroSecreto) {
-        printf("parabéns você acertou!\n");
-    }
-    else if (palpite > numeroSecreto) {
-        printf("muito alto! tente mais baixo\n");
-    }
-    else {
-        printf("muito baixo! tente mais alto\n");
+
+        if (palpite == numeroSecreto) {
+            printf("parabens voce acertou!\n");
+        } 
+        else if (palpite > numeroSecreto) {
+            printf("muito alto! tente mais baixo\n");
+        } 
+        else {
+            printf("muito baixo! tente mais alto\n");
+        }
+
     } while (palpite != numeroSecreto && tentativas < 10);
-        if (palpite != numeroSecreto) {
-            printf("uma pena, você perdeu");
-        }
-        
+
+    if (palpite != numeroSecreto) {
+        printf("uma pena, voce perdeu!\n");
     }
-        }
+
     return 0;
+}
